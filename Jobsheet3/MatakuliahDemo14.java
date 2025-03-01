47e60da349776696b1f659e4f8a51fbc6bdfd371
adamarsyadfaizin <adamkian09@gmail.com>
2025-03-01 16:50:02 +0000

    Percobaan3 Pertanyaan

--- Jobsheet3/MatakuliahDemo14.java
+++ Jobsheet3/MatakuliahDemo14.java
@@ -3,34 +3,22 @@ import java.util.Scanner;
 public class MatakuliahDemo14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah14[] arrayOfMatakuliah = new Matakuliah14 [3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
 
-        for(int i=0; i < 3; i++) {
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jumlahMatkul = Integer.parseInt(sc.nextLine());
+
+        Matakuliah14[] arrayOfMatakuliah = new Matakuliah14[jumlahMatkul];
+
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode      : ");
-            kode = sc.nextLine();
-            System.out.print("Nama      : ");
-            nama = sc.nextLine();
-            System.out.print("Sks       : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam: ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("-------------------------------");
-
-            arrayOfMatakuliah[i] = new Matakuliah14(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah[i] = new Matakuliah14();
+            arrayOfMatakuliah[i].tambahData();
         }
 
-        for(int i=0; i < 3; i++) {
+        System.out.println("\n===== DATA MATAKULIAH =====");
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
-            System.out.println("Kode        : "+ arrayOfMatakuliah[i].kode);
-            System.out.println("Nama        : "+ arrayOfMatakuliah[i].nama);
-            System.out.println("Sks         : "+ arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah Jam  : "+ arrayOfMatakuliah[i].jumlahJam);
-            System.out.println("------------------------------");
+            arrayOfMatakuliah[i].cetakInfo();
         }
     }
 }
