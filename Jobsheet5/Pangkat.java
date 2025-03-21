f3161662ab4261422a08976cef630263455de08d
adamarsyadfaizin <adamkian09@gmail.com>
2025-03-21 10:46:44 +0700

    Commit

---
+++ Jobsheet5/Pangkat.java
@@ -0,0 +1,27 @@
+public class Pangkat {
+
+    int nilai, pangkat;
+    Pangkat(int n, int p){
+        nilai=n;
+        pangkat=p;
+    }
+    public int pangkatBF() {
+        int hasil = 1;
+        for (int i = 0; i < pangkat; i++) {
+            hasil *= nilai;
+        }
+        return hasil;
+    }    
+    int pangkatDC(int a, int n){
+        if (n==1) {
+            return a;
+        }else{
+            if (n%2==1) {
+                return(pangkatDC(a, (int)n/2)*pangkatDC(a, (int)n/2)*a);
+            }else{
+                return(pangkatDC(a, n/2)*pangkatDC(a, n/2));
+            }
+        }
+    }
+}
+    
