d747366244651b4c34adfe24bffb3f30fe87c2ea
adamarsyadfaizin <adamkian09@gmail.com>
2025-04-14 13:35:32 +0000

    Add files via upload

---
+++ Jobsheet7/Dosen14.java
@@ -0,0 +1,18 @@
+public class Dosen14 {
+    String kode;
+    String nama;
+    boolean jenisKelamin;
+    int usia;
+
+    public Dosen14(String kd, String name, boolean jk, int age) {
+        this.kode = kd;
+        this.nama = name;
+        this.jenisKelamin = jk;
+        this.usia = age;
+    }
+
+    public void tampil() {
+        System.out.println("Kode: " + kode + ", Nama: " + nama + ", Jenis Kelamin: " 
+            + (jenisKelamin ? "Laki-laki" : "Perempuan") + ", Usia: " + usia);
+    }
+}
